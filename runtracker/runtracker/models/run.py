da79ce807c45b93f76a7169bbc21525b7710fc05
buzz1274 <david@sulaco.co.uk>
2016-07-05 10:46:31 +0100

    add run

--- runtracker/runtracker/models/run.py
+++ runtracker/runtracker/models/run.py
@@ -14,17 +14,31 @@ class Run(models.Model):
     date =  models.DateField(null=False, blank=False)
     second = models.IntegerField(null=False, blank=False)
     distance = models.IntegerField(blank=False, null=False)
+    complete = models.BooleanField(blank=False, null=False, default=True)
 
 
     class Meta:
         db_table = 'run'
+        ordering = ['date', 'run_id']
 
-    def duration(self):
-        return "%s:%s:%s" % (str(self.second / 3600).zfill(2),
-                             str((self.second % 3600) / 60).zfill(2),
-                             str((self.second % 3600) % 60).zfill(2) ,)
+    def duration(self, seconds = None):
+        if seconds:
+            seconds = int(seconds)
+        else:
+            seconds = int(self.second)
+
+        return "%s:%s:%s" % (str(seconds / 3600).zfill(2),
+                             str((seconds % 3600) / 60).zfill(2),
+                             str((seconds % 3600) % 60).zfill(2) ,)
 
     def km(self):
         return "{0:.3f}".format(float(self.distance) / 1000)
+
+    def min_per_km(self):
+        return (float(self.second) / 60) /\
+               (float(self.distance) / 1000)
+
+    def five_k_time(self):
+        return self.duration(self.min_per_km() * 5 * 60)
 
 admin.site.register(Run)
