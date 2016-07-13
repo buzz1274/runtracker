3ab853adf144a28a5232a2856d9ca0fe197ef7fa
buzz1274 <david@sulaco.co.uk>
2016-07-13 21:23:17 +0100

    knockout

---
+++ rt/Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+    require('jit-grunt')(grunt);
+
+    grunt.loadNpmTasks('grunt-webpack');
+
+    var webpack = require("webpack");
+    var webpackConfig = require("./webpack.config.js");
+
+    grunt.initConfig({
+        webpack: {
+            options: webpackConfig,
+            build: {
+                plugins: webpackConfig.plugins.concat(
+                    new webpack.optimize.DedupePlugin(),
+                    new webpack.optimize.UglifyJsPlugin(),
+                    new webpack.DefinePlugin({
+                            'process.env': {
+                                'NODE_ENV': '"production"'
+                            }
+                        }
+                    ))
+            },
+            build_dev: {
+                debug: true
+            }
+        },
+        watch: {
+            app: {
+                files: ["app/**/*{.js,.css,.less,.jsx}", "public/**/*.html"],
+                tasks: ["webpack:build_dev"],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("dev", ["webpack:build_dev", "watch:app"]);
+    grunt.registerTask("build", ["webpack:build"]);
+
+};
---
+++ rt/webpack.config.js
@@ -0,0 +1,30 @@
+var webpack = require('webpack'),
+    ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './knockout/app.js',
+    output: {
+        path: './public/js/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.html$/, loader: 'html' },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel'
+            }
+        ]
+    },
+    externals: {
+        'ko': 'ko'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            jQuery: "jquery",
+            $: "jquery"
+        }),
+        new ExtractTextPlugin("main.css", {allChunks: true})
+    ]
+};
