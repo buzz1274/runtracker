f5f7154f7d118c251cb257d70c1c5e77e3da0275
buzz1274 <david@sulaco.co.uk>
2016-10-17 22:47:42 +0100

    db migrations@

--- database/migrations/2016_10_13_222439_create_activity_table.php
+++ database/migrations/2016_10_13_222439_create_activity_table.php
@@ -14,7 +14,15 @@ class CreateActivityTable extends Migration
     {
         Schema::create('activity', function (Blueprint $table) {
             $table->increments('id');
-
+            $table->unsignedSmallInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedSmallInteger('activity_id');
+            $table->foreign('activity_id')->references('id')->on('activity');
+            $table->unsignedSmallInteger('route_id');
+            $table->foreign('route_id')->references('id')->on('route');
+            $table->date('activity_date');
+            $table->unsignedSmallInteger('seconds');
+            $table->unsignedSmallInteger('metres');
         });
     }
 
