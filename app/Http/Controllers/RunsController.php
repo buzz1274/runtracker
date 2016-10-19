202ac70304d979ab9883ed27db18b82d0faaeb7a
buzz1274 <david@sulaco.co.uk>
2016-10-20 00:01:18 +0100

    heroku

--- app/Http/Controllers/RunsController.php
+++ app/Http/Controllers/RunsController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request as Request;
 class RunsController extends Controller {
 
     public function index(Request $request) {
-        return response()->json(Run::where('who', 'david')->
-                    orderBy('date', 'DESC')->limit(10)->get());
     }
 
 }
