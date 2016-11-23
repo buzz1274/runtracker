5d329f8cfbefa2a1d588d872d8f4c03a73aa1ca4
buzz1274 <david@sulaco.co.uk>
2016-11-23 16:36:05 +0000

    db import into heroku

---
+++ import_db_into_heroku.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ ! -f "runtracker.sql" ]; then
+    echo "runtracker.sql does not exist"
+fi
+
+export `heroku config -s`
+
+heroku run php artisan migrate:refresh
+sudo docker cp runtracker.sql postgres:/tmp/runtracker.sql
+sudo docker exec -it postgres bash -c "export PGPASSWORD=$DB_PASSWORD; psql -U$DB_USERNAME -h$DB_HOST $DB_DATABASE < /tmp/runtracker.sql"
+heroku run php artisan db:convert
