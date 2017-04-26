259f8ec5fe1497279f9aadbfcedcd8a43e555d41
buzz1274 <david@sulaco.co.uk>
2017-04-26 17:41:39 +0100

    .

--- knockout/models/activity_types.js
+++ knockout/models/activity_types.js
@@ -14,7 +14,6 @@ var page = require('page'),
               dataType: 'json',
               async: true,
         success: function(data) {
-          console.log(data);
           that.activity_types = data;
         },
         error: function() {
